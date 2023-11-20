d34c5396710e5e480d31db964d21d8f4e0a06d07
uwuelias <eliaswu466@gmail.com>
2023-11-19 23:40:48 -0500

    will add more pages in the future

---
+++ script.js
@@ -0,0 +1,186 @@
+const defaultpic = document.getElementById('default');
+const blackdefault = document.getElementById('black-default');
+const bluedefault = document.getElementById('blue-default');
+const mintdefault = document.getElementById('mint-default');
+const pinkdefault = document.getElementById('pink-default');
+const purpledefault = document.getElementById('purple-default');
+const whitedefault = document.getElementById('white-default');
+const yellowdefault = document.getElementById('yellow-default');
+
+blackdefault.addEventListener('click', function() {
+    defaultpic.src='default/black.png';
+})
+bluedefault.addEventListener('click', function() {
+    defaultpic.src='default/blue.png';
+})
+mintdefault.addEventListener('click', function() {
+    defaultpic.src='default/mint.png'
+})
+pinkdefault.addEventListener('click', function() {
+    defaultpic.src='default/pink.png'
+})
+purpledefault.addEventListener('click', function() {
+    defaultpic.src='default/purple.png'
+})
+whitedefault.addEventListener('click', function() {
+    defaultpic.src='default/white.png'
+})
+yellowdefault.addEventListener('click', function() {
+    defaultpic.src='default/yellow.png'
+})
+
+
+const catyarn = document.getElementById('cat-yarn');
+const blackyarn = document.getElementById('black-cat-yarn');
+const blueyarn = document.getElementById('blue-cat-yarn');
+const mintyarn = document.getElementById('mint-cat-yarn');
+const pinkyarn = document.getElementById('pink-cat-yarn');
+const purpleyarn = document.getElementById('purple-cat-yarn');
+const whiteyarn = document.getElementById('white-cat-yarn');
+const yellowyarn = document.getElementById('yellow-cat-yarn');
+
+blackyarn.addEventListener('click', function() {
+    catyarn.src='cat on yarn ball/black yarn.png'
+})
+blueyarn.addEventListener('click', function() {
+    catyarn.src='cat on yarn ball/blue yarn.png'
+})
+mintyarn.addEventListener('click', function() {
+    catyarn.src='cat on yarn ball/mint yarn.png'
+})
+pinkyarn.addEventListener('click', function() {
+    catyarn.src='cat on yarn ball/pink yarn.png'
+})
+purpleyarn.addEventListener('click', function() {
+    catyarn.src='cat on yarn ball/purple yarn.png'
+})
+whiteyarn.addEventListener('click', function() {
+    catyarn.src='cat on yarn ball/white yarn.png'
+})
+yellowyarn.addEventListener('click', function() {
+    catyarn.src='cat on yarn ball/yellow yarn.png'
+})
+
+const catnoodle = document.getElementById('cat-noodle');
+const noodleblack = document.getElementById('black-noodle');
+const noodleblue = document.getElementById('blue-noodle');
+const noodlemint = document.getElementById('mint-noodle');
+const noodlepink = document.getElementById('pink-noodle');
+const noodlepurple = document.getElementById('purple-noodle');
+const noodlewhite = document.getElementById('white-noodle');
+const noodleyellow = document.getElementById('yellow-noodle');
+
+noodleblack.addEventListener('click', function() {
+    catnoodle.src='cat with noodle/black noodle.png'
+})
+noodleblue.addEventListener('click', function() {
+    catnoodle.src='cat with noodle/blue noodle.png'
+})
+noodlemint.addEventListener('click', function() {
+    catnoodle.src='cat with noodle/mint noodle.png'
+})
+noodlepink.addEventListener('click', function() {
+    catnoodle.src='cat with noodle/pink noodle.png'
+})
+noodlepurple.addEventListener('click', function() {
+    catnoodle.src='cat with noodle/purple noodle.png'
+})
+noodlewhite.addEventListener('click', function() {
+    catnoodle.src='cat with noodle/white noodle.png'
+})
+noodleyellow.addEventListener('click', function() {
+    catnoodle.src='cat with noodle/yellow noodle.png'
+})
+
+const cats = document.getElementById('cats');
+const blackcats = document.getElementById('black-cats');
+const bluecats = document.getElementById('blue-cats');
+const mintcats = document.getElementById('mint-cats');
+const pinkcats = document.getElementById('pink-cats');
+const purplecats = document.getElementById('purple-cats');
+const whitecats = document.getElementById('white-cats');
+const yellowcats = document.getElementById('yellow-cats');
+
+blackcats.addEventListener('click', function() {
+    cats.src='cats/blackcats.png'
+})
+bluecats.addEventListener('click', function() {
+    cats.src='cats/bluecats.png'
+})
+mintcats.addEventListener('click', function() {
+    cats.src='cats/mintcats.png'
+})
+pinkcats.addEventListener('click', function() {
+    cats.src='cats/pinkcats.png'
+})
+purplecats.addEventListener('click', function() {
+    cats.src='cats/purplecats.png'
+})
+whitecats.addEventListener('click', function() {
+    cats.src='cats/whitecats.png'
+})
+yellowcats.addEventListener('click', function() {
+    cats.src='cats/yellowcats.png'
+})
+
+const christmashk = document.getElementById('christmas-hk');
+const blackchristmas = document.getElementById('black-christmas-hk');
+const bluechristmas = document.getElementById('blue-christmas-hk');
+const mintchristmas = document.getElementById('mint-christmas-hk');
+const pinkchristmas = document.getElementById('pink-christmas-hk');
+const purplechristmas = document.getElementById('purple-christmas-hk');
+const whitechristmas = document.getElementById('white-christmas-hk');
+const yellowchristmas = document.getElementById('yellow-christmas-hk');
+
+blackchristmas.addEventListener('click', function() {
+    christmashk.src='christmas hello kitty/black christmas.png'
+})
+bluechristmas.addEventListener('click', function() {
+    christmashk.src='christmas hello kitty/blue christmas.png'
+})
+mintchristmas.addEventListener('click', function() {
+    christmashk.src='christmas hello kitty/mint christmas.png'
+})
+pinkchristmas.addEventListener('click', function() {
+    christmashk.src='christmas hello kitty/pink christmas.png'
+})
+purplechristmas.addEventListener('click', function() {
+    christmashk.src='christmas hello kitty/purple christmas.png'
+})
+whitechristmas.addEventListener('click', function() {
+    christmashk.src='christmas hello kitty/white christmas.png'
+})
+yellowchristmas.addEventListener('click', function() {
+    christmashk.src='christmas hello kitty/yellow christmas.png'
+})
+
+const fish = document.getElementById('fish');
+const  blackfish = document.getElementById('blackfish');
+const  bluefish = document.getElementById('bluefish');
+const  mintfish = document.getElementById('mintfish');
+const  pinkfish = document.getElementById('pinkfish');
+const  purplefish = document.getElementById('purplefish');
+const  whitefish = document.getElementById('whitefish');
+const  yellowfish = document.getElementById('yellowfish');
+
+blackfish.addEventListener('click', function() {
+    fish.src='fish/blackfish.jpg'
+})
+bluefish.addEventListener('click', function() {
+    fish.src='fish/bluefish.jpg'
+})
+mintfish.addEventListener('click', function() {
+    fish.src='fish/mintfish.jpg'
+})
+pinkfish.addEventListener('click', function() {
+    fish.src='fish/pinkfish.jpg'
+})
+purplefish.addEventListener('click', function() {
+    fish.src='fish/purplefish.jpg'
+})
+whitefish.addEventListener('click', function() {
+    fish.src='fish/whitefish.jpg'
+})
+yellowfish.addEventListener('click', function() {
+    fish.src='fish/yellowfish.jpg'
+})
